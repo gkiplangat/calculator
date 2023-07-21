b2062dc591457af279c54dff66bee10e631f9217
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-07-22 00:36:09 +0300

    feat:setting variables

---
+++ index.js
@@ -0,0 +1,27 @@
+let numEl = 0
+let result = 0 
+function num_one() {
+    numEl = 1
+    sum += numEl
+     
+    console.log(sum)
+}
+function num_two() {
+    numEl = 2
+    sum += numEl
+    
+    console.log(sum)
+}
+function num_three() {
+    numEl = 3
+    sum += numEl
+   
+    console.log(sum)
+    
+}
+function plus() {
+    numEl = document.getElementById("plus").textContent
+    console.log(numEl)
+}
+result = numEl + sign + numEl
+
